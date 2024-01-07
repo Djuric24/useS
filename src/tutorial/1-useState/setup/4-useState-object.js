48ef7f1a582fe07c4e14e84bba56448f4ffc012a
djuric24 <djuric24a@gmail.com>
2024-01-07 17:12:56 +0100

    multi

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -4,16 +4,22 @@ const UseStateObject = () => {
   const [person, setPerson] = useState({
     name:'John', age:22, message: 'new message' 
   });
+
+  const [name,setName] = useState('Peter');
+  const [age,setAge] = useState('24');
+  const [message,setMessage] = useState('random message');
+
   const changeMessage = () => {
-    setPerson({...person, message:'Hello World'});
+    // setPerson({...person, message:'Hello World'});
+    setMessage('hello world')
   };
 
   return (
     <>
       <h2>useState object example</h2>;
-      <h2>{person.name}</h2>
-      <h2>{person.age}</h2>
-      <h2>{person.message}</h2>
+      <h2>{name}</h2>
+      <h2>{age}</h2>
+      <h2>{message}</h2>
       <button className='btn' onClick={changeMessage} > change message</button>
     </>
 )
