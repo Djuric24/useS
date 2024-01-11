475cf8edb32eebd8d8bb8bdc12a2fd20ab9c9443
djuric24 <djuric24a@gmail.com>
2024-01-11 22:02:17 +0100

    show/hide

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import Setup from './tutorial/3-conditional-rendering/setup/2-short-circuit';
+import Setup from './tutorial/3-conditional-rendering/setup/3-show-hide';
 
-// 52. multi returns gotova, ali 53. multireturns fatching nije ti jasna!!!
-// ide ternary 55.
+// 56. show-hide gotova, ali 53. multireturns fatching nije ti jasna!!!
+// ide  57.
 
 function App() {
   return (
