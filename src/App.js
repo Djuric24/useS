e2287af7cc15d7e1dff2f029adc2826f3638dd6a
djuric24 <djuric24a@gmail.com>
2024-01-11 21:41:16 +0100

    ternary

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Setup from './tutorial/3-conditional-rendering/setup/2-short-circuit';
 
 // 52. multi returns gotova, ali 53. multireturns fatching nije ti jasna!!!
-// 
+// ide ternary 55.
 
 function App() {
   return (
